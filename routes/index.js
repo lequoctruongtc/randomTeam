a474aefe94926600b3922f41e2d98fb3102e2503
Le Quoc Truong <truong.lq@tctav.com>
2017-10-30 16:34:36 +0700

    [UPDATE] get member and team from firebase

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,72 @@
 var express = require('express');
 var router = express.Router();
 
+var firebase = require('firebase');
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+  apiKey: "AIzaSyA4h7ZKyG88pHnmTP84ns4arGQg7ZQwEbs",
+  authDomain: "randomteam-75929.firebaseapp.com",
+  databaseURL: "https://randomteam-75929.firebaseio.com",
+  storageBucket: "randomteam-75929.appspot.com",
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+// Get list member from firebase
+function getListMember() {
+  return new Promise(function(resolve, reject) {
+    var listMember = database.ref('/member');
+    listMember.once('value').then(function(snapshot) {
+      var data = snapshot.val();
+      return data;
+    });
+  });
+}
+
+// Create member after random team
+function createMember(members) {
+  members.forEach(function(element) {
+    database.ref('/member').set({
+      name: element
+    });
+  }, this);
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  let urlTeam = database.ref('/team');
+  let urlMember = database.ref('/member');
+  urlTeam.once('value').then(function(snapshot) {
+    return snapshot.val();
+  }).then(function(team){
+    urlMember.once('value', function(snapshot){
+      let listMember = snapshot.val();
+      res.render('index', { title: 'Express', team: team, listMember: listMember });
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+  }).catch(console.error.bind(console));
+
+  // let member = database.ref('/member');
+  // member.once('value', function(snapshot) {
+  //   let listMember = snapshot.val();
+  //   res.render('index', { title: 'Express', listMember: listMember });
+  // }, function (errorObject) {
+  //   console.log("The read failed: " + errorObject.code);
+  // });
 });
 
+// Random member for teams
 router.post('/', function(req, res, next) {
-  let members = req.body.listMember.split("\r\n").map(function (val) {
-      return val;
-  });
+  // let members = req.body.listMember.split("\r\n").map(function (val) {
+  //     return val;
+  // });
+  let members = req.body.listMember;
   members = members.filter(String).sort(function(a, b){return 0.5 - Math.random()});
   let numberOfTeam = req.body.teams;
   let teamMembers = {};
@@ -26,6 +83,8 @@ router.post('/', function(req, res, next) {
     currentMemberIndex++;
     currentTeamIndex = (++currentTeamIndex >= numberOfTeam) ?  0 : currentTeamIndex;
   }
+  // console.log(teamMembers);
+  // createMember(teamMembers);
   res.render('result', { title: 'Express', teamMembers: teamMembers });
 });
 
